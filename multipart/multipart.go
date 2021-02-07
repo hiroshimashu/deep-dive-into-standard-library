def07fd132f2fd1686b1f56a765e690c3752997b
hiroshimashu <hiroshimashu@gmail.com>
2021-02-07 15:03:36 +0900

    add: Part type

--- multipart/multipart.go
+++ multipart/multipart.go
@@ -1,8 +1,29 @@
 package multipart
 
+import (
+	"io"
+	"net/textproto"
+)
+
 var emptyParams = make(map[string]string)
 // TODO 
 // goのmakeの仕様に目を通す
 // mapもsyntaxと使い方を復習する
 
 const peekBufferSize = 4096
+
+type Part struct {
+	Header textproto.MIMEHeader
+
+	mr *Reader
+
+	disposition string 
+	dispositionParams map[string]string
+
+	r io.Reader
+
+	n int 
+	total int64
+	err error
+	readErr error
+}
