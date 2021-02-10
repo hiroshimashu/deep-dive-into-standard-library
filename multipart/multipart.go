8367c7140e8e0bf942c5cac5453cf308833b7d5a
hiroshimashu <hiroshimashu@gmail.com>
2021-02-10 15:55:38 +0900

    add: method for Reader

--- multipart/multipart.go
+++ multipart/multipart.go
@@ -1,6 +1,9 @@
 package multipart
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"mime"
 	"net/textproto"
@@ -63,4 +66,102 @@ func (p *Part) FormName() string {
 
 func (p *Part) Read(d []byte) (n int, err error) {
 	return p.r.Read(d)
+}
+
+type Reader struct {
+	bufReader *bufio.Reader
+
+	currentPart *Part 
+	partsRead int 
+
+	nl []byte 
+	nlDashBoundary []byte
+	dashBoundaryDash []byte
+	dashBoundary []byte
+}
+
+func (r *Reader) NextPart() (*Part, error) {
+	return r.nextPart(false)
+}
+
+func (r *Reader) nextPart(rawPart bool) (*Part, error) {
+	r.currentPart != nil {
+		r.currentPart.Close()
+	}
+	if string(r.dashBoundary) == "--" {
+		return nil, fmt.Errorf("multipart: boundary is empty")
+	}
+	expectNewPart := false
+
+	for {
+		line, err := r.bufReader.ReadSlice('\n')
+
+		if err == io.EOF && r.isFinaleBoundary(line) {
+			return nil, io.EOF
+		}
+		if err != nil {
+			return nil, fmt.Errorf("mutipart: NextPart: %v", err)
+		}
+
+		if r.isBoundaryDelimiterLine(line) {
+			r.partsRead++
+			bp, err := newPart(r, rawPart)
+			if err != nil {
+				return nil, err
+			}
+			r.currentPart = bp
+			return bp, nil
+		}
+
+		if r.partsRead == 0 {
+			continue
+		}
+
+		return nil, fmt.Errorf("multipart: unexpected line in Next(): %q", line)
+	}
+}
+
+func (mr *Reader) isBoundaryDelimiterLine(line []byte) (ret bool) {
+	if !bytes.HasPrefix(line, mr.dashBoundary) {
+		return false
+	}
+	rest := line[len(mr.dashBoundary):]
+	rest = skipLWSPChar(rest)
+
+	if mr.partsRead == 0 && len(rest) == 1 && rest[0] == '\n' {
+		mr.nl = mr.nl[1:]
+		mr.nlDashBoundary = mr.nlDashBoundary[1:]
+	}
+	return bytes.Equal(rest, mr.nl)
+}
+
+func skipLWSPChar(b []byte) []byte {
+	for len(b) > 0 && (b[0] == ' ' || b[0] == '\t') {
+		b = b[1:]
+	}
+	return b
+}
+
+func NewReader(r io.Reader, boundary string) *Reader {
+	b := []byte("\r\n--" + boundary + "--")
+	return &Reader{
+		bufReader: bufio.NewReaderSize(&stickyErrorReader{r: r}, peekBufferSize),
+		nl: b[:2],
+		nlDashBoundary: b[:len(b)-2],
+		dashBoundaryDash: b[2:],
+		dashBoundary: b[2: len(b)-2 ],
+	}
+}
+
+type stickyErrorReader struct {
+	r io.Reader
+	err error 
+}
+
+func (r *stickyErrorReader) Read(p []byte) (n int, _ error) {
+	if r.err != nil {
+		return 0, r.err
+	}
+	n, r.err = r.r.Read(p)
+	return n, r.err
 }
